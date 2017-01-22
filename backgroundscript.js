52a3119a6989d2c90799a71bcc0c96c9fd423d24
mwarrenus <mwarren_us@yahoo.com>
2017-01-21 22:19:11 -0800

    Add smile.amazon.com to filter and extend url.(match|replace) regexp.

--- backgroundscript.js
+++ backgroundscript.js
@@ -1,9 +1,9 @@
 function remove_affiliate_links( details ) {
-	if ( details.url.match( /&tag=[a-zA-Z0-9\-]*/ ) != null) {
+	if ( details.url.match( /[?&]tag=[\w-]+/ ) != null) {
 		return {
-			redirectUrl: details.url.replace( /(&tag=[a-zA-Z0-9\-]*)/, '' )
+			redirectUrl: details.url.replace( /([?&]tag=[\w-]+)/, '' )
 		};
 	}
 }
 
-chrome.webRequest.onBeforeRequest.addListener( remove_affiliate_links, { urls: ["*://www.amazon.com/*", "*://www.amazon.co.uk/*", "*://amazon.com/*", "*://amazon.co.uk/*"] }, ["blocking"] );
+chrome.webRequest.onBeforeRequest.addListener( remove_affiliate_links, { urls: ["*://www.amazon.com/*", "*://www.amazon.co.uk/*", "*://amazon.com/*", "*://amazon.co.uk/*", "*://smile.amazon.com/*"] }, ["blocking"] );
